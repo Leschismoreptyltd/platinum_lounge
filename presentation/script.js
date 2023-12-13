c275022effd7fdf15d8bc6a27ccd6072e21e90ac
Damian Lesch <135703455+Leschismoreptyltd@users.noreply.github.com>
2023-12-13 20:57:56 +0200

    Add files via upload

--- presentation/script.js
+++ presentation/script.js
@@ -9,33 +9,3 @@ window.addEventListener('scroll', function () {
     }
   });
 
-  let currentSlide = 1;
-
-  //slide show
-function showSlide(n) {
-  const slides = document.getElementsByClassName('carousel-slide');
-  
-  if (n > slides.length) {
-    currentSlide = 1;
-  } else if (n < 1) {
-    currentSlide = slides.length;
-  }
-
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-
-  slides[currentSlide - 1].style.display = 'block'; 
-}
-
-function changeSlide(n) {
-  showSlide(currentSlide += n);
-}
-
-// Auto slide every 3 seconds (adjust the interval as needed)
-setInterval(() => {
-  changeSlide(1);
-}, 3000);
-
-// Initial display
-showSlide(currentSlide);
